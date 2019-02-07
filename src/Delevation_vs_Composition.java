01f090a77b9a6a0919cbdaa5bbc8ffeae87c07c4
Dmitriy <diman240798@yandex.ru>
2019-02-07 22:03:23 +0300

    Strategy

---
+++ src/Delevation_vs_Composition.java
@@ -0,0 +1,44 @@
+public class Delevation_vs_Composition {
+    /*
+    * Делегирование - прямое имплементирование (субклассирование)
+    * при этом поведение задается внутри объекта
+    *
+    * Композиция - задание поведение через объект (абстракцию)
+    * соответсвует паттерну Стратегия
+    * */
+
+    public static void main(String[] args) {
+        SomeBehaviour someBehaviourDelegation = new Delegation();
+        someBehaviourDelegation.behaveSomeHow();
+
+        SomeBehaviour someBehaviourComposition = () -> System.out.println("Behave via Composition");
+        Strategy_Composition strategy_composition = new Strategy_Composition(someBehaviourComposition);
+        strategy_composition.getSomeBehaviour().behaveSomeHow();
+    }
+}
+interface SomeBehaviour {
+    void behaveSomeHow();
+}
+
+class Delegation implements SomeBehaviour{
+    @Override
+    public void behaveSomeHow() {
+        System.out.println("Behave via DELEGATION");
+    }
+}
+
+class Strategy_Composition {
+    private SomeBehaviour someBehaviour;
+
+    public Strategy_Composition(SomeBehaviour someBehaviour) {
+        this.someBehaviour = someBehaviour;
+    }
+
+    public SomeBehaviour getSomeBehaviour() {
+        return someBehaviour;
+    }
+
+    public void setSomeBehaviour(SomeBehaviour someBehaviour) {
+        this.someBehaviour = someBehaviour;
+    }
+}
